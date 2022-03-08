297d246ed1f7a6ff415845580e0f5b19dc7613a2
Adam Watts <a_watts@outlook.com>
2022-03-08 12:30:04 -0400

    removed default from app.js, extended login form

--- src/App.js
+++ src/App.js
@@ -5,21 +5,6 @@ import LoginWindow from './Components/Login/login';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-
-      </header>
       <LoginWindow />
     </div>
   );
--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -1,16 +1,26 @@
-// TODO: add functionality to link to the game display once teh submit button is clicked.
+// TODO: add functionality to link to the game display once the submit button is clicked.
 
 const LoginWindow = function(props)
 {
 
     return(
+        <div>
+            <h1>Honey Badger Games!</h1>
+            <p>New Player:</p>
+                <form id="newUserCreation">
+                    <label for="newUserName">Please Enter Your Username:</label><br></br>
+                    <input type="text" id="newUserName" name="newUserName"></input><br></br>
+                    <input type="submit" value="Create New User"></input>
+                </form>
 
-        <form id="loginPage">
-        <label for="username">Please Enter your Username:</label><br></br>
-        <input type="text" id="username" name="username"></input><br></br>
-        <input type="submit"></input>
-        </form>
+            <p>Already a Player:</p>
+                <form id="loginForm">
+                    <label for="existingUserName">Please Enter Your Username:</label><br></br>
+                    <input type="text" id="existingUserName" name="existingUserName"></input><br></br>
+                    <input type="submit" value="Login"></input>
+                </form>
 
+        </div>
     )
 }
 
