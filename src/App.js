291daae5d2370f26ce71b217bc178e8ca63d349a
Stratton <W0401926@campus.nscc.ca>
2022-03-09 10:02:11 -0400

    updated favicon image, added searchuser component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import LoginWindow from './Components/Login/login';
+import UserSearch from './Components/UserSearch/user-search';
 
 function App() {
   return (
     <div className="App">
       <LoginWindow />
+      <UserSearch />
     </div>
   );
 }
