c9cb8a90878494c4e72e34e008a1659f86f9724c
AWatts0 <a_watts@outlook.com>
2022-03-09 12:02:22 -0400

    changes to login and app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import LoginWindow from './Components/Login/login';
 import GameDisplay from "./Components/GameDisplay/game-display"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import {BrowserRouter, Routes, Route} from "react-router-dom"
 
 import UserSearch from './Components/UserSearch/user-search';
 
 function App() {
   return (
-    <div className="App">
-      <header className='App-header'>
-        <GameDisplay />
-        <LoginWindow />
-      </header>
-    </div >
+    <div className="App-header">
+      <Routes>
+        <Route path="/" element={<LoginWindow/>}></Route>
+        <Route path="/game-display" element={<GameDisplay/>}></Route>
+      </Routes>
+    </div>
   );
 }
 
--- src/Components/Login/login.js
+++ src/Components/Login/login.js
@@ -9,7 +9,7 @@ const LoginWindow = function(props)
             <h1>Honey Badger Games!</h1>
             <p>New Player:</p>
                 <form id="newUserCreation">
-                    <label for="newUserName">Please Enter Your Username:</label><br></br>
+                    <label htmlFor="newUserName">Please Enter Your Username:</label><br></br>
                     <input type="text" id="newUserName" name="newUserName"></input><br></br>
                     <span>Upload your Profile Picture: </span> <input type="button" id="userIcon" value="Select your Image"></input><br></br>
                     <input type="submit" value="Create New User"></input>
@@ -17,9 +17,9 @@ const LoginWindow = function(props)
             <br></br>
             <p>Already a Player:</p>
                 <form id="loginForm">
-                    <label for="existingUserName">Please Enter Your Username:</label><br></br>
+                    <label htmlFor="existingUserName">Please Enter Your Username:</label><br></br>
                     <input type="text" id="existingUserName" name="existingUserName"></input><br></br>
-                    <Link to ="/game-display"><button type="button" value="Login"></button></Link>
+                    <Link to ="/game-display"><button type="button" value="Login">Login</button></Link>
                 </form>
 
         </div>
