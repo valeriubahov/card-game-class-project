5e1078845bb247723e2646f735d86d06dde1060d
Grace Stratton <W0401926@campus.nscc.ca>
2022-03-09 11:49:53 -0400

    setting up environment for adding user search

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import LoginWindow from './Components/Login/login';
 import GameDisplay from "./Components/GameDisplay/game-display"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
-import UserSearch from './Components/UserSearch/user-search';
-
 function App() {
   return (
     <div className="App">
