a6303d1e13081871fed02077795e3590e3101544
Mykyta <mykyta.y@hotmail.com>
2022-03-24 12:09:28 -0300

    Started working on popup box with round results

---
+++ src/Components/PopUp/popUp.js
@@ -0,0 +1,11 @@
+//TODO: Pop up box will appear at the end of the game
+//      informing who won the game
+//      button to go to next round
+
+const PopUp = function(props){
+    return(
+        <div className="popup-box">
+            
+        </div>
+    )
+}
